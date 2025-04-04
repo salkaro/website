814ee1ba7096fe0cf93e72e42b45672526d27479
RoyalGr4pe <njames.programming@gmail.com>
2025-04-04 19:57:35 +0100

    add navbar code demo

--- src/app/code-demos/page.tsx
+++ src/app/code-demos/page.tsx
@@ -7,7 +7,8 @@ import Layout from "@/components/layout/Layout";
 export default function CodeDemos() {
     return (
         <Layout>
-            <div>
+            <div className="flex flex-col gap-2 items-center justify-center text-center">
+                <Link href="./code-demos/navbar">Navbar</Link>
                 <Link href="./code-demos/footer">Footer</Link>
             </div>
         </Layout>
